cef014c9b664b8344ba614c538cb2369776eeb34
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-07-09 11:45:52 +0600

    ok.

---
+++ src/redux/fearures/counter/counterSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type InitialStateType = {
+  count: number;
+};
+
+const initialState: InitialStateType = {
+  count: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    },
+  },
+});
+
